cc629a416e71b09a2dbcb32194de2b4aa87ae10d
b7s9 <interactive@b7s9.com>
2021-02-16 15:28:33 -0500

    🐛 Fix error if no swipe target

--- js/swipe-handler.js
+++ js/swipe-handler.js
@@ -2,18 +2,21 @@ import TinyGesture from './TinyGesture.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const target = document.getElementById('gesture-target');
-  const gesture = new TinyGesture(target);
+  if (target) {
 
-  gesture.on('swipeleft', event => {
-    // The gesture was a left swipe.
+    const gesture = new TinyGesture(target);
 
-    // This will always be true for a left swipe.
-    gesture.swipedHorizontal;
-    // This will be true if diagonalSwipes is on and the gesture was diagonal
-    // enough to also be a vertical swipe.
-    gesture.swipedVertical;
+    gesture.on('swipeleft', event => {
+      // The gesture was a left swipe.
 
-    console.log('swiped')
-  });
+      // This will always be true for a left swipe.
+      gesture.swipedHorizontal;
+      // This will be true if diagonalSwipes is on and the gesture was diagonal
+      // enough to also be a vertical swipe.
+      gesture.swipedVertical;
+
+      console.log('swiped')
+    });
+  }
 
 })
