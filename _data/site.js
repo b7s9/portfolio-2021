5c96e8b12e774e416084e392b839879d9ff95475
b7s9 <interactive@b7s9.com>
2021-02-16 21:35:48 -0500

    🐛 Fix remote links for testing

--- _data/site.js
+++ _data/site.js
@@ -1,17 +1,17 @@
 
 const site = {
-	"title": "Ben Schenerman",
-	"description": "Ben Schenerman's Portfolio"
+  "title": "Ben Schenerman",
+  "description": "Ben Schenerman's Portfolio"
 };
 
 module.exports = function () {
-	if (process.env.ELEVENTY_PRODUCTION) {
-		site.baseURL = 'https://b7s9.com/';
-	} else if (process.env.ELEVENTY_URL) {
-		site.baseURL = process.env.ELEVENTY_URL
-	} else {
-		site.baseURL = 'http://localhost:8080';
-	}
+  if (process.env.ELEVENTY_PRODUCTION) {
+    site.baseURL = 'https://b7s9.com/test/';
+  } else if (process.env.ELEVENTY_URL) {
+    site.baseURL = process.env.ELEVENTY_URL
+  } else {
+    site.baseURL = 'http://localhost:8080';
+  }
 
-	return site;
+  return site;
 };
