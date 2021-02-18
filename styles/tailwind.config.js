2e4b2a322c18bb4cafab2e0ddfbcaa90deb61b37
b7s9 <interactive@b7s9.com>
2021-02-18 15:50:05 -0500

    🔧 Add utility divs acting as paragraphs 🔧 Add fonts into tailwind config

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
   },
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['Overpass, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'],
+      'serif': ['Piazzolla, ui-serif, Georgia, Cambria, "Times New Roman", Times, serif'],
+      'mono': ['Menlo, ui-monospace, SFMono-Regular, Monaco, Consolas, "Ubuntu Mono", "Liberation Mono", "Courier New", monospace']
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
