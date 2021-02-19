4c6e1caadd7306ef63af91112ff0a793ed4bbeb5
b7s9 <interactive@b7s9.com>
2021-02-19 18:47:18 -0500

    🐛 Fix font paths

--- _data/site.js
+++ _data/site.js
@@ -6,7 +6,7 @@ const site = {
 
 module.exports = function () {
   if (process.env.ELEVENTY_PRODUCTION) {
-    site.baseURL = 'https://b7s9.com/test';
+    site.baseURL = 'https://dev.b7s9.com/portfolio-2021';
   } else if (process.env.ELEVENTY_URL) {
     site.baseURL = process.env.ELEVENTY_URL
   } else {
