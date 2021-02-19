d0d1513661ac6f72220a0e39a33d217588700a88
b7s9 <interactive@b7s9.com>
2021-02-19 16:01:52 -0500

    🔧 Use neutral gray

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
       current: 'currentColor',
       black: colors.black,
       white: colors.white,
-      gray: colors.coolGray,
+      gray: colors.gray,
       green: colors.green,
       red: colors.rose,
       primary: {
