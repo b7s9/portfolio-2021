bc4bf0051e6335c86c7e670f83f04565fc4ad783
b7s9 <interactive@b7s9.com>
2021-02-19 15:05:42 -0500

    ✨ Add complete project filtering :)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// ------------------------------------------------
+// PROJECT GALLERY/TEXT CONTENT HANDLERS
+// ------------------------------------------------
 b7s9 = {
   textContent: document.getElementById('text-content'),
   galleryContent: document.getElementById('gallery-content'),
@@ -34,6 +37,8 @@ let galleryIndicator = b7s9.galleryIndicator
 if (textIndicator) {
   // init recolor
   b7s9.updateActiveColor()
+  document.onkeydown = checkKey;
+
 
   textIndicator.addEventListener('click', e => {
     b7s9.showTextContent()
@@ -45,8 +50,6 @@ if (galleryIndicator) {
     b7s9.showGallery()
   })
 }
-
-document.onkeydown = checkKey;
 
 function checkKey(e) {
 
@@ -61,27 +64,99 @@ function checkKey(e) {
 
 }
 
+// ------------------------------------------------
+// FILTERS
+// ------------------------------------------------
 
-// ------------------------
+let filterState = {
+  ux: false, ui: false, dev: false, audio: false
+}
 
-function filters() {
-  return {
-    ux: true, ui: true, dev: true, audio: true,
+// ---------------------
+// VIEW
+// ---------------------
+
+const projectPreviews = document.getElementsByClassName('project-preview')
+
+function updateProjectDisplay() {
+  for (project of projectPreviews) {
+
+    if (isFilterStateEmpty()) { // if no filters are active, show all projects
+      showProject(project)
+      continue;
+    }
+
+    const filterString = project.getAttribute('data-filters')
+    const projectFilters = filterString.split(',')
+    // console.log(project)
+    let anyMatch = 0;
+
+    for (filter of projectFilters) {
+      // console.log(filter)
+
+      if (filterState[filter]) {
+        anyMatch = 1;
+      }
+    }
+    if (anyMatch) {
+      showProject(project)
+    } else {
+      hideProject(project)
+    }
   }
 }
 
-// const projectPreviews = document.getElementsByClassName('project-preview')
-// if (projectPreviews.length > 0) {
-//   for (project of projectPreviews) {
-//     const filterString = project.getAttribute('data-filters')
-//     const projectFilters = filterString.split(',')
-//     console.log(project)
-//     for (filter of projectFilters) {
-//       console.log(filter)
-//     }
-//   }
-// }
+function hideProject(project) {
+  project.classList.add('hidden')
+}
 
-// function filterChange(filter){
+function showProject(project) {
+  project.classList.remove('hidden')
+}
 
-// }
+function activateButton(button) {
+  button.classList.add('text-gray-800', 'border-green-500', 'text-primary-light')
+  button.classList.remove('text-gray-600', 'border-gray-300')
+}
+function deactivateButton(button) {
+  button.classList.remove('text-gray-800', 'border-green-500', 'text-primary-light')
+  button.classList.add('text-gray-600', 'border-gray-300')
+}
+
+// ---------------------
+// MODEL
+// ---------------------
+
+function updateFilterState(button) {
+  filterDelta = button.getAttribute('data-filter-name')
+
+  filterState[filterDelta] = !filterState[filterDelta];
+
+  if (filterState[filterDelta]) {
+    activateButton(button)
+  } else {
+    deactivateButton(button)
+  }
+
+  updateProjectDisplay()
+}
+
+function isFilterStateEmpty() {
+  let isEmptyState = 1;
+  for (let [key, value] of Object.entries(filterState)) {
+    if (value) isEmptyState = 0;
+  }
+  return isEmptyState;
+}
+
+// ---------------------
+// HANDLER
+// ---------------------
+
+const filterButtons = document.querySelectorAll('.filters button');
+
+for (button of filterButtons) {
+  button.addEventListener('click', e => {
+    updateFilterState(e.target)
+  })
+}
