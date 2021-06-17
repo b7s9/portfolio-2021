ed4353f0f68fb6ad5e365043414a77c51b5c307d
b7s9 <interactive@b7s9.com>
2021-06-17 10:03:50 -0400

    🐛 Reset content prefs token for everyone

--- js/content-prefs.js
+++ js/content-prefs.js
@@ -1,8 +1,11 @@
 // const lsNamespace = 'b7s9portfolio2021' // received from main.js
 
 document.addEventListener('DOMContentLoaded', () => {
-  const isReturnVisitor = b7s9.lsNamespace + 'visited'
-  if (localStorage.getItem(isReturnVisitor) == 0) {
+  // purge old token
+  localStorage.removeItem(b7s9.lsNamespace + 'visited')
+  // use new token
+  const isReturnVisitor = b7s9.lsNamespace + 'visited-v1'
+  if (!localStorage.getItem(isReturnVisitor)) {
     localStorage.setItem(b7s9.prefersGalleryFirst, 0)
     b7s9.showContentPrefs()
     localStorage.setItem(isReturnVisitor, 1)
