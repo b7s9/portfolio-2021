46f67192ca9615079238cac8e21bc0364efa28e5
b7s9 <interactive@b7s9.com>
2021-06-17 12:09:13 -0400

    🔧 Purge content prefs token

--- js/content-prefs.js
+++ js/content-prefs.js
@@ -1,10 +1,11 @@
 // const lsNamespace = 'b7s9portfolio2021' // received from main.js
 
 document.addEventListener('DOMContentLoaded', () => {
-  // purge old token
+  // purge old tokens
   localStorage.removeItem(b7s9.lsNamespace + 'visited')
+  localStorage.removeItem(b7s9.lsNamespace + 'visited-v1')
   // use new token
-  const isReturnVisitor = b7s9.lsNamespace + 'visited-v1'
+  const isReturnVisitor = b7s9.lsNamespace + 'visited-v2'
   if (!localStorage.getItem(isReturnVisitor)) {
     localStorage.setItem(b7s9.prefersGalleryFirst, 0)
     b7s9.showContentPrefs()
